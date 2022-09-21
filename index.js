27aeb2896fa501ff36e4b667b83f7c5c2cd2b179
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-09-20 23:07:02 -0600

    Realiza ejercicio de la sección 15 Persistencia de datos de Open BootCamp

---
+++ index.js
@@ -0,0 +1,19 @@
+let nombre = "Jimmy"
+let apellido = "Bustillo"
+
+let objeto = {
+  nombre: "Jimmy",
+  apellido: "Bustillo"
+}
+
+// sessionStorage.setItem('objeto', JSON.stringify(objeto))
+
+// localStorage.setItem('objeto', JSON.stringify(objeto))
+
+let fecha = new Date('September 20, 2022 23:00:00').toUTCString()
+
+
+// document.cookie = `objetoCookie=${JSON.stringify(objeto)};expires=${fecha}`
+
+// let minutos = new Date().getUTCMinutes() + 2
+// console.log(minutos);
